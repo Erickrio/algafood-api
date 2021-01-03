944549596eeb464a7686993e93a858811725d0d2
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-03 16:58:10 -0300

    adicionando dependencia obrigarotia !

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -7,38 +7,20 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 @Component
 public class AtivacaoClienteService {
-	
-	//ponto de injecao - atributo (AS VEZES É USADO AQUI ESSA INJEÇÃO - AUTOWIRED)
+	//dependencia obrigatoria - sem isso n funciona.
 	@Autowired
 	private Notificador notificador;
-	
-	/*1 ponto de injeção - construtor (IDEAL É QUE USE A INJEÇÃO DE DEPENDENCIA AQUI)
-	@Autowired //2 ponto de injeção - @Autowired define qual construtor deve usar.
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-	} */
-	
-	/*
-	public AtivacaoClienteService(String exemplo) {
-		//construtor exemplo - spring  não consegue identificar qualé o padrão
-	}*/
 	
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
+		
+		if(notificador != null) {
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+	}else {
+		System.out.println("Não existe notificador,mas o cliente foi notificado");
 	}
 
-	public Notificador getNotificador() {
-		return notificador;
-	}
-
-	/*
-	//3 ponto de injecao - setetter - precisa por anotação Autowired,se não dá nullpoienter
-	@Autowired
-	public void setNotificador(Notificador notificador) {
-		this.notificador = notificador;
-	} */
-	
+}
 
 }
