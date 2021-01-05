a993a780fd9bb57bb8f069874fd255c4236af29a
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-04 21:05:40 -0300

    add comportamento usando profile

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -4,22 +4,21 @@ package com.algaworks.algafood.di.notificacao;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import com.algaworks.algafood.di.modelo.Cliente;
 
-@Profile("prod")
+import com.algaworks.algafood.di.modelo.Cliente;
+@Profile("dev")
 @TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 @Component
-public class NotificadorEmail implements Notificador {
+public class NotificadorEmailMock implements Notificador {
 	
-	public NotificadorEmail() {
-		System.out.println("NotificadorEmail REAL");
+	public NotificadorEmailMock() {
+		System.out.println("NotificadorEmail MOCK");
 	}
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		System.out.printf("Notificando %s através do e-mail %s: %s\n", 
+		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
-
 
 }
