9f269292298f9782ceae92e1e3ad1557e85aa3db
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-04 21:25:53 -0300

    ciclo de vida do bean - init e destroy

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.di.service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,14 +10,29 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.NivelUrgencia;
 import com.algaworks.algafood.di.notificacao.Notificador;
 import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
-@Component
+//@Component
 public class AtivacaoClienteService {
 //problema de ambuiguidade é que o spring n sabe qual que executa (email ou sms)??
 
-	@TipoDoNotificador(NivelUrgencia.URGENTE)
+	@TipoDoNotificador(NivelUrgencia.SEM_URGENCIA)
 	@Autowired
 	private Notificador notificador;
+	//1 - CICLO DE VIDA /  É CHAMADO DEPOIS DO CONSTRUTOR
 	
+//	@PostConstruct  
+//	public void init() {
+//		System.out.println("INIT...." + notificador);
+//	}
+	
+	public void init() {
+		System.out.println("INIT...." + notificador);
+	}
+	
+	//antes de ser destroido é chamado o bean
+	//@PreDestroy
+	public void destroy() {
+		System.out.print("DESTROY");
+	}
 	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
