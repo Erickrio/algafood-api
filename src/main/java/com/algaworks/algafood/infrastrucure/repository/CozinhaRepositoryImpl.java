2fb9cd22d0b72829e055b9d52e78fd15c55f420e
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-10 21:50:09 -0300

    conhecendo e implementando o padrao repository

--- src/main/java/com/algaworks/algafood/infrastrucure/repository/CozinhaRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastrucure/repository/CozinhaRepositoryImpl.java
@@ -1,4 +1,6 @@
-package com.algaworks.algafood.jpa;
+package com.algaworks.algafood.infrastrucure.repository;
+
+import java.util.List;
 
 import java.util.List;
 
@@ -6,37 +8,39 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.model.Cozinha;
-
-@Component
-public class CadastroCozinha {
-
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+@Repository
+public class CozinhaRepositoryImpl implements CozinhaRepository{
+	
 	@PersistenceContext
 	private EntityManager manager;
 	
+	@Override
 	public List<Cozinha> listar() {
 		return manager.createQuery("from Cozinha", Cozinha.class)
 				.getResultList();
 	}
 	
-	//retorna uma unica cozinha - busca pelo ID.
+	@Override
 	public Cozinha buscar(Long id) {
 		return manager.find(Cozinha.class, id);
 	}
 	
-	@Transactional//executado dentro de uma transação
+	@Transactional
+	@Override
 	public Cozinha salvar(Cozinha cozinha) {
 		return manager.merge(cozinha);
 	}
 	
 	@Transactional
+	@Override
 	public void remover (Cozinha cozinha) {
-		//busca o contexto da persistencia(intancia gerenciada)
 		cozinha = buscar(cozinha.getId());
-		//chaa o remove
 		manager.remove(cozinha);
 	}
-	
+
 }
--- src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/algaworks/algafood/jpa/AlteracaoCozinhaMain.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationContext;
 
 import com.algaworks.algafood.AlgafoodApiApplication;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 public class AlteracaoCozinhaMain {
 	
@@ -15,8 +16,8 @@ public class AlteracaoCozinhaMain {
 		ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
 				.web(WebApplicationType.NONE)
 				.run(args);
-		
-		CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+		//instancia de cozinha repository
+		CozinhaRepository cadastroCozinha = applicationContext.getBean(CozinhaRepository.class);
 		
 		Cozinha cozinha = new Cozinha();
 		cozinha.setId(1l);
