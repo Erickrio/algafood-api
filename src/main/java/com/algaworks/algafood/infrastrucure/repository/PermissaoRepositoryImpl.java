b0d2e7b4c7cb678875d1fd9440b22a95b63c8d3f
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-19 20:15:25 -0300

    criando as interfaces dos repositorios

--- src/main/java/com/algaworks/algafood/infrastrucure/repository/PermissaoRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastrucure/repository/PermissaoRepositoryImpl.java
@@ -1,5 +1,42 @@
 package com.algaworks.algafood.infrastrucure.repository;
 
-public class PermissaoRepositoryImpl {
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.model.Permissao;
+import com.algaworks.algafood.domain.repository.PermissaoRepository;
+
+public class PermissaoRepositoryImpl implements PermissaoRepository {
+	
+	@PersistenceContext
+	private EntityManager manager;
+
+	@Override
+	public List<Permissao> listar() {
+		return manager.createQuery("from Permissao",Permissao.class).getResultList();
+	}
+
+	@Override
+	public Permissao buscar(Long id) {
+		return manager.find(Permissao.class, id);
+	}
+
+	@Override
+	@Transactional
+	public Permissao salvar(Permissao permissao) {
+		return manager.merge(permissao);
+	}
+
+	@Override
+	@Transactional
+	public void remover(Permissao permissao) {
+		permissao = buscar(permissao.getId());
+		manager.remove(permissao);
+
+	}
 
 }
