84abaf5b3186c6cf60ae2946df7cb3eb8bedcd46
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-19 21:18:58 -0300

    add xml no pom e entendo o produces = json ou xml

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,13 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping("/cozinhas")
+@RequestMapping(value= "/cozinhas") // produces = MediaType.APPLICATION_JSON_VALUE) //	//produz formato especifico JSON
 public class CozinhaController {
 	
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
-	@GetMapping
+
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<Cozinha> listar(){
 		return cozinhaRepository.listar();
 	}
