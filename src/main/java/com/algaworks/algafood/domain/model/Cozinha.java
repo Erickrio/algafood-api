4f76e3d6119f349ddd3746a9cea820d0c5b792ec
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-20 21:17:16 -0300

    customizando os nomes das representaçoes

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -7,10 +7,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-
+@JsonRootName("gastronomia")
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true) 
 @Entity
@@ -21,6 +25,8 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	//@JsonIgnore// ignora o titulo - Representacao
+	@JsonProperty("titulo") //não muda o nome do modelo de dominio mas sim a REPRESENTAÇÃO
 	@Column(nullable = false)
 	private String nome;
 }
