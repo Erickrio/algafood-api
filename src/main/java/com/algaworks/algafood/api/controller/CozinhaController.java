26fc2be315807f87f942cfc16365e393f67ecd90
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-21 20:26:34 -0300

    customizando xml c wrapper e jackson

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,33 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
-@RequestMapping(value= "/cozinhas") 
+@RequestMapping(value = "/cozinhas")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-
+	
 	@GetMapping
-	public List<Cozinha> listar(){
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
 	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}
+
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long  cozinhaId) {
-		//busca 1 unica cozinha p/ id e retorna 
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
 		return cozinhaRepository.buscar(cozinhaId);
-		
 	}
 
 }
--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -2,31 +2,27 @@ package com.algaworks.algafood.domain.model;
 
 
 import javax.persistence.Column;
-import javax.persistence.Entity; //javax vem do JPA
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@JsonRootName("gastronomia")
+@JsonRootName("cozinha")
 @Data
-@EqualsAndHashCode(onlyExplicitlyIncluded = true) 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
-	@EqualsAndHashCode.Include 
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	//@JsonIgnore// ignora o titulo - Representacao
-	@JsonProperty("titulo") //não muda o nome do modelo de dominio mas sim a REPRESENTAÇÃO
 	@Column(nullable = false)
 	private String nome;
 }
