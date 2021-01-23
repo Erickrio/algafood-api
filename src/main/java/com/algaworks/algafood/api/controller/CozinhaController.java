5810a92c6af62682b19e91454db3b2f543069f54
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-23 11:16:11 -0300

    corrigindo o status htttp para resource inexistente

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,8 +3,10 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,11 +33,21 @@ public class CozinhaController {
 	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
-	@ResponseStatus(HttpStatus.CREATED)
+	//responseEntity - manipula a respota
 	@GetMapping("/{cozinhaId}")
-	public Cozinha buscar(@PathVariable Long cozinhaId) {
-		return cozinhaRepository.buscar(cozinhaId);
+	public ResponseEntity <Cozinha>buscar(@PathVariable Long cozinhaId) {
+		Cozinha cozinha =  cozinhaRepository.buscar(cozinhaId);
+		
+		//se a cozinha existe no repositorio 
+		if(cozinha != null) {
+			//retorna ok - corpo da reposta a representação da cozinha
+			return ResponseEntity.ok(cozinha); 
+		}
+		
+		//return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		
+		//retorna s corpo - notFound
+		return ResponseEntity.notFound().build(); 
 	}
 
 }
