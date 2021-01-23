b50819aa9e09aea34c16341a51f862bc85062ee6
erick Monteiro <monteiro.ck@hotmail.com>
2021-01-23 12:08:14 -0300

    modelando e implementando endpoint de novo recurso post

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -9,6 +9,8 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +50,13 @@ public class CozinhaController {
 		
 		//retorna s corpo - notFound
 		return ResponseEntity.notFound().build(); 
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED) 
+	public void adicionar(@RequestBody Cozinha cozinha) {
+		cozinhaRepository.salvar(cozinha);
+		
 	}
 
 }
