9e784bdf869e62c8acdf3d3470575aadbee9bf11
erick Monteiro <monteiro.ck@hotmail.com>
2021-04-24 14:30:13 -0300

    add restrições na clausula where com criterio API

--- src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/domain/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -54,7 +56,19 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 		
 	
 		CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-		criteria.from(Restaurante.class);
+		Root<Restaurante> root = criteria.from(Restaurante.class);
+		
+		//criando instancia de predicate
+		
+		Predicate nomePredicate = builder.like(root.get("nome"), "%" + nome + "%" );
+		Predicate taxaInicialPredicate = builder
+				.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+		Predicate taxaFinalPredicate = builder
+				.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+		
+		//busca só por nome
+		criteria.where(nomePredicate,taxaInicialPredicate,taxaFinalPredicate); 
+		
 		TypedQuery<Restaurante> query = manager.createQuery(criteria);
 		return query.getResultList();
 		
