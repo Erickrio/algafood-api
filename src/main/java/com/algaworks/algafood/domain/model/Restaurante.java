d30fa3f2fd9693e36f64f2de117eee44f77a9639
erick Monteiro <monteiro.ck@hotmail.com>
2021-05-08 12:41:19 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany - chaves compostas

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,6 +1,8 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -8,6 +10,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
 import lombok.Data;
@@ -32,6 +36,15 @@ public class Restaurante {
 	@ManyToOne //muitos restaurantes possui uma cozinha
 	@JoinColumn (name="cozinha_id",nullable = false)// restrições para não permitir valores nulos nos atributos de restaurante
 	private Cozinha cozinha;
+	
+	//muitos restaurantes possui muitas formas de pagamento
+	//joincolumn- define o nome da coluna da tab intermediaria da tab restaurante
+    //inverso - tab forma de Pagamento
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento",
+			joinColumns = @JoinColumn(name = "restaurante_id"),
+			inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento>formasPagamento = new ArrayList<>();
 
 	
 	
