22f222652ad8a3520ac678a0ab29012ea5d6897e
erick Monteiro <monteiro.ck@hotmail.com>
2021-05-08 11:51:25 -0300

    Mapeando relacionamento muitos-para-muitos

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,12 +1,17 @@
 package com.algaworks.algafood.domain.model;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 
 import lombok.Data;
@@ -25,4 +30,12 @@ public class Cozinha {
 	
 	@Column(nullable = false)
 	private String nome;
+	
+	//1 cozinha tem muitos restaurantes
+	// new ArrayList<>(); - inicializa como uma lista fazia para evitar nullpointer
+	//mappedBy - Em restaurante qual propriedade que mapea x'a cozinha
+	//jsonIgonre - ignora a serialização -loop
+	@JsonIgnore
+	@OneToMany(mappedBy="cozinha")
+	private List<Restaurante>restaurantes = new ArrayList<>();
 }
