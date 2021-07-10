3fb82daa88c168765675fcfb5a1c6c7f28b21da1
erick Monteiro <monteiro.ck@hotmail.com>
2021-07-10 11:25:22 -0300

    Simplificando o código com o uso de @ResponseStatus em exception

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -41,14 +41,10 @@ public class CozinhaController {
 	}
 	
 	@GetMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
-		Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
 		
-		if (cozinha.isPresent()) {
-			return ResponseEntity.ok(cozinha.get());
-		}
-		
-		return ResponseEntity.notFound().build();
+		return cadastroCozinha.buscarOuFalhar(cozinhaId);
+
 	}
 	
 	@PostMapping
@@ -58,34 +54,15 @@ public class CozinhaController {
 	}
 	
 	@PutMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId,
-			@RequestBody Cozinha cozinha) {
-		Optional<Cozinha> cozinhaAtual = cozinhaRepository.findById(cozinhaId);
-		
-		if (cozinhaAtual.isPresent()) {
-			BeanUtils.copyProperties(cozinha, cozinhaAtual.get(), "id");
-			
-			Cozinha cozinhaSalva = cadastroCozinha.salvar(cozinhaAtual.get());
-			return ResponseEntity.ok(cozinhaSalva);
-		}
-		
-		return ResponseEntity.notFound().build();
+	public Cozinha atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+
+		Cozinha cozinhaAtual = cadastroCozinha.buscarOuFalhar(cozinhaId);
+		BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+		return cadastroCozinha.salvar(cozinhaAtual);
+
 	}
-//	
-//	@DeleteMapping("/{cozinhaId}")
-//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId) {
-//		try {
-//			cadastroCozinha.excluir(cozinhaId);	
-//			return ResponseEntity.noContent().build();
-//			
-//		} catch (EntidadeNaoEncontradaException e) {
-//			return ResponseEntity.notFound().build();
-//			
-//		} catch (EntidadeEmUsoException e) {
-//			return ResponseEntity.status(HttpStatus.CONFLICT)
-//					.body(e.getMessage());
-//		}
-//	}
+	
 	
 //	@DeleteMapping("/{cozinhaId}")
 //	@ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -3,20 +3,17 @@ package com.algaworks.algafood.domain.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
-//se excecão for lançada e nao tratada nos queremos que o retorno seja 404
-//@ResponseStatus(value = HttpStatus.NOT_FOUND) //,reason="Entidade Não encontrada") 
-public class EntidadeNaoEncontradaException extends ResponseStatusException {
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND) 
+public class EntidadeNaoEncontradaException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 	
-	public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
-		super(status, mensagem);
+	public EntidadeNaoEncontradaException( String mensagem) {
+		super(mensagem);
 		// TODO Auto-generated constructor stub
 	}
 
 	
-	public EntidadeNaoEncontradaException(String mensagem) {
-		this(HttpStatus.NOT_FOUND,mensagem);
-	}
 	
 }
